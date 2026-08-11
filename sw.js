01e50e055f496807a7b15f94a8f7a3da4bee4b1b
tomocchio_ <baltjetuk@gmail.com>
2026-08-11 18:01:03 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,22 +1,37 @@
-const CACHE_NAME = 'zen-gantt-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+const CACHE_NAME = 'zen-gantt-v2'; // Ubah versi ini jika Anda ingin memaksa update baru
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName); // Hapus cache lama otomatis
+                    }
+                })
+            );
+        })
+    );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        fetch(event.request)
+            .then((response) => {
+                // Jika online, ambil dari jaringan dan update cache
+                const responseClone = response.clone();
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, responseClone);
+                });
+                return response;
+            })
+            .catch(() => {
+                // Jika offline, baru ambil dari cache
+                return caches.match(event.request);
+            })
+    );
 });
